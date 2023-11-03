1cf2c0ee6855c6f2b10303890a150531aa027eeb
Suhas <suhas.g@cashfree.com>
2023-11-03 16:54:03 +0530

    v3-sdk-setuo

--- interface/__init__.py
+++
@@ -1,5 +0,0 @@
-
-
-from cashfree_pg_sdk_python.interface.cfconfig import CFConfig
-from cashfree_pg_sdk_python.interface.cfheaders import CFHeaders
-from cashfree_pg_sdk_python.interface.cferror import CFError
--- interface/cfheaders.py
+++
@@ -1,4 +0,0 @@
-class CFHeaders:
-    def __init__(self, x_idempotency_key=None, x_request_id=None):
-        self.x_idempotency_key = x_idempotency_key
-        self.x_request_id = x_request_id
