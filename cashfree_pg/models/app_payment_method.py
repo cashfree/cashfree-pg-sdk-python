f788f8cd71eb31c9d9aefa167f1c52738549e645
Dom <4187791+domjv@users.noreply.github.com>
2025-11-21 14:11:52 +1000

    shim: add app_payment_method model from backup

---
+++ cashfree_pg/models/app_payment_method.py
@@ -0,0 +1,61 @@
+from __future__ import annotations
+import pprint
+import json
+
+
+from pydantic import BaseModel, Field, ConfigDict
+from cashfree_pg.models.app import App
+
+class AppPaymentMethod(BaseModel):
+    """
+    App payment method
+    """
+    app: App = Field(...)
+    __properties = ["app"]
+
+    model_config = ConfigDict(populate_by_name=True, validate_assignment=True)
+    def to_str(self) -> str:
+        """Returns the string representation of the model using alias"""
+        return pprint.pformat(self.model_dump(by_alias=True))
+
+    def to_json(self) -> str:
+        """Returns the JSON representation of the model using alias"""
+        return json.dumps(self.to_dict())
+
+    @classmethod
+    def from_json(cls, json_str: str) -> AppPaymentMethod:
+        """Create an instance of AppPaymentMethod from a JSON string"""
+        return cls.from_dict(json.loads(json_str))
+    
+    @classmethod
+    def from_json_for_one_of(cls, json_str: str) -> AppPaymentMethod:
+        """Create an instance of AppPaymentMethod from a JSON string"""
+        temp_dict = json.loads(json_str)
+        if "app" in temp_dict.keys():
+            return cls.from_dict(json.loads(json_str))
+        return None
+
+    def to_dict(self):
+        """Returns the dictionary representation of the model using alias"""
+        _dict = self.model_dump(by_alias=True,
+                          exclude={
+                          },
+                          exclude_none=True)
+        # override the default output from pydantic by calling `to_dict()` of app
+        if self.app:
+            _dict['app'] = self.app.to_dict()
+        return _dict
+
+    @classmethod
+    def from_dict(cls, obj: dict) -> AppPaymentMethod:
+        """Create an instance of AppPaymentMethod from a dict"""
+        if obj is None:
+            return None
+
+        if not isinstance(obj, dict):
+            return AppPaymentMethod.model_validate(obj)
+
+        _obj = AppPaymentMethod.model_validate({
+            "app": App.from_dict(obj.get("app")) if obj.get("app") is not None else None
+        })
+        return _obj
